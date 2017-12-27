39bcedc404c253c4e5478f4775aa068c5c9c8ac0
Barret J. Nobel <bear.nobel@gmail.com>
2017-12-27 10:59:15 -0500

    updated implementation of all commands. Add / delete /l ist tasks on the server

--- AdvancedNode/server.js
+++ AdvancedNode/server.js
@@ -3,6 +3,8 @@ const EventEmitter = require('events');
 class Server extends EventEmitter {
     constructor(client) {
         super();
+        this.tasks = {}; // holds task info
+        this.taskID = 1; // holds current Task ID
 
         process.nextTick(() => {
             this.emit('response', 'Type a command');
@@ -24,20 +26,29 @@ class Server extends EventEmitter {
         });
     }
 
+    tasksString() {
+        return Object.keys(this.tasks).map(key => {
+            return `${key}: ${this.tasks[key]}`;
+        }).join('\n');
+    }
+
     help() {
         this.emit('response', 'Available Commands: add task\nls\ndelete: id');
     }
 
     add(args) {
-        this.emit('response', args.join(' '));
+        this.tasks[this.taskID] = args.join(' ');
+        this.emit('response', `Added Task ${this.taskID}`);
+        this.taskID++;
     }
 
     ls() {
-        this.emit('response', 'ls ...');
+        this.emit('response', `Tasks:\n${this.tasksString()}`);
     }
 
-    delete() {
-        this.emit('response', 'delete ...');
+    delete(args) {
+        delete (this.tasks[args[0]]);
+        this.emit('response', `Deleted task ${args[0]}`);
     }
 
 }
